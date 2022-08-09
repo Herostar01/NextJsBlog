a1dc189e97f99c49aa1637cc07e848a1865e6d5d
Muhammad Shahbaz <M023Shah@gmail.com>
2022-08-08 23:22:23 -0700

    adding pages

--- writtenword/pages/posts/[slug].js
+++ writtenword/pages/posts/[slug].js
@@ -1,8 +1,10 @@
 
 
-import styles from '../styles/Slug.module.css'
+import styles from '../../styles/Slug.module.css'
 
 import {GraphQLClient, gql} from 'graphql-request'
+
+import moment from 'moment';
 
 
 const graphcms = new GraphQLClient("https://api-us-west-2.hygraph.com/v2/cl6fw6sf22pjo01unhk0ydsv5/master")
@@ -24,17 +26,62 @@ const QUERY = gql `
             content{
                 html
             }
+            coverPhoto{
+                id
+                url
+            }
         }
     }
 `;
 
+const SLUGLIST = gql`
+    {
+        posts {
+            slug
+        }
+    }
+`;
 
-export async function getStaticProps(){
-  const {posts} = await graphcms.request(QUERY);
-  return{
+export async function getStaticPaths() {
+    const { posts } = await graphcms.request(SLUGLIST);
+    
+    return {
+        paths: posts.map((post) => ({ params: { slug: post.slug } })), 
+        fallback: false,
+    };
+}
+
+
+
+
+export async function getStaticProps({ params }){
+    const slug = params.slug;
+    const data = await graphcms.request(QUERY, {slug});
+    const post = data.post;
+    
+    return{
     props: {
-      posts,
+      post,
     },
     revalidate: 10,
   };
+}
+
+export default function BlogPost({ post }){
+    return(
+        <main className={styles.blog}>
+            <img src={post.coverPhoto} alt="" 
+            className={styles.cover}
+            />
+
+            <div className={styles.title} >
+                <img src={post.author.avatar.url} alt=""  />
+                <div className={styles.authtext} > 
+                    <h6> By {post.author.name} </h6>
+                    <h6 className={styles.date} > {post.datePublished} </h6>
+                </div>
+            </div>
+            <h2> {post.title} </h2>
+        </main>
+    )
 }
